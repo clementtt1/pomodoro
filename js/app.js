0341d41036b36883720054c89800febe1800e78c
Clément <schneid223@etu.unicaen.fr>
2023-09-18 08:08:50 +0200

    v2 sans CSS

--- js/app.js
+++ js/app.js
@@ -1,33 +1,55 @@
-let work_countdown = 1500
+let work_countdown = document.getElementById("work_time")
 let break_countdown = 300
+let ID = 0
 
 const timeSpan = document.getElementById("time")
+const startButton = document.getElementById("start")
+const workDisplay = document.getElementById("work")
+const breakDisplay = document.getElementById("break")
 
-setInterval(() => {
-    //countdown of work time or for break time
-    if(work_countdown > 0){
-        work_countdown -= 1
-        display(work_countdown)
+start.addEventListener("click", e => isRunning())
+
+function isRunning(){
+    work_countdown -= 1
+    display(work_countdown)
+    if (startButton.textContent == "Start") {
+        startButton.textContent = "Reset"
+            ID = setInterval(() => {
+
+            //countdown of work time or for break time
+            if(work_countdown >= 0){
+                work_countdown -= 1
+                display(work_countdown)
+
+            }
+                
+            else if(break_countdown >= 0){
+                break_countdown -= 1
+                display(break_countdown)
+            }
+                
+            else {
+                work_countdown = 1500
+                display(work_countdown)
+                break_countdown = 300
+                display(break_countdown)
+            }
+
+            console.log(work_countdown, break_countdown)
+        }, 1000)
     }
-        
-    else if(break_countdown > 0){
-        break_countdown -= 1
-        display(break_countdown)
-    }
-        
-    else {
+    else{
+        clearInterval(ID)
+        startButton.textContent = "Start"
         work_countdown = 1500
-        display(work_countdown)
         break_countdown = 300
-        display(break_countdown)
+        display(work_countdown)
     }
-
-    console.log(work_countdown, break_countdown)
-}, 1000)
+}
 
 function translationSecondsIntoMinuts(time) {
     let minuts = 0
-    while(time > 60){
+    while(time >= 60){
         time -= 60
         minuts += 1
     }
